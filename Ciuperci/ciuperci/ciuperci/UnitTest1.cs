f6da3a1cb9d0da1f393e6b79aeaefa5517676e5e
flaviu83 <juniormind2016@gmail.com>
2016-06-03 15:10:05 +0300

    ciuperci

---
+++ Ciuperci/ciuperci/ciuperci/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ciuperci
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        int Calculateciuperci(int n, int a, int b, int x)
+        {
+            return b = (x + 1 - n * n) / n;
+        }
+    }
+}
