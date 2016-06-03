48aae310f97de8aa07fbc4754c9e616929c39a81
flaviu83 <juniormind2016@gmail.com>
2016-06-03 14:01:19 +0300

    Muscasitrenurile

--- Musca_si_Trenurile1/Musca_si_Trenurile/Musca_si_Trenurile1.cs
+++ Musca_si_Trenurile1/Musca_si_Trenurile/Musca_si_Trenurile1.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace Musca_si_Trenurile
 {
@@ -9,6 +12,18 @@ namespace Musca_si_Trenurile
         [TestMethod]
         public void TestMethod1()
         {
+          
+        }
+        int Calculatekmmusca(int t1, int t2,int d,int k)
+        {
+
+
+            return  k = d - t1 - t2;
+            //t1=t2=viteza trenurilor; iar "d" este distanta totala parcusa; 
+            //k=km parcusi de musca acesta fiind km ramasi dintre trenuri;
+            //asta deoarece viteza trenurilor adunate este egala cu viteza cu care zboara musca iar in timpul ramas zboara aceeasi distanta.
+
         }
     }
 }
+
---
+++ Win32Project2/Win32Project2/calc.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	int t, l, d, k;
+	cout << "t=";
+	cin >> t;
+	cout << "l=";
+	cin >> l ;
+	cout << "d=";
+	cin >> d;
+	k = d - t - l;
+	cout << "Km Parcursi de musca=" << k << endl;
+}
