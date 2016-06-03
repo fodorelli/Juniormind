d30dcc8667dc5e81d1d25d02574d533b22e980de
flaviu83 <juniormind2016@gmail.com>
2016-06-03 14:54:24 +0300

    Problema cu Capre

---
+++ Capre/capre/capre/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace capre
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        decimal Calculatecapre(int z, int x, int y, decimal k, int q, int w)
+        {
+            return k=(z / x / y)*q*w;
+           
+            //x=nr de zile; y=nr de capre;z=nr kg de fan;
+            //q=nr de capre din problema; w=nr de zile ;k=nr total de kg de fan
+
+        }
+    }
+}
