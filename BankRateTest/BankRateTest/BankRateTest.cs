a0b9b4b8f366d7008e84bcf6822405636f1a0672
flaviu83 <juniormind2016@gmail.com>
2016-06-07 17:42:46 +0300

    BankRateTests

---
+++ BankRateTest/BankRateTest/BankRateTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRateTest
+{
+    [TestClass]
+    public class BankRateTest
+    {
+        [TestMethod]
+        public void RateForFirstMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 1);
+            Assert.AreEqual(102, rate);
+
+        }
+        public void RateForSecondMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 2);
+            Assert.AreEqual(101, rate);
+        }
+        decimal CalculateBankRate(decimal total, int periodInMonths, decimal interestPerYear, int currentMonth)
+
+        {
+            decimal principal = total / periodInMonths;
+            decimal exactInterestPerMonth = interestPerYear / 12 / 100;
+            return principal + total * exactInterestPerMonth;
+        }
+
+    }
+}
