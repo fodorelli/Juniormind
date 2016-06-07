91832fd0c77d46f8de866c299401217165ad2391
flaviu83 <juniormind2016@gmail.com>
2016-06-07 17:59:27 +0300

    Sportsman

---
+++ Sportsman/Sportsman/sportsman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportsman
+{
+    [TestClass]
+    public class sportsman
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+                        int repetitions = NumberOfRepetitions(5);
+                        Assert.AreEqual(25, repetitions);
+        }
+        int NumberOfRepetitions(int numberpreparationrounds)
+        {
+            return numberpreparationrounds * numberpreparationrounds;
+        }
+    }
+}
