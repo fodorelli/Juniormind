adaa8647d4d4fa95ff71029afb39e3a90522e6ca
flaviu83 <juniormind2016@gmail.com>
2016-06-07 12:20:27 +0300

    FlyingBird Project

--- FlyingBird/FlyingBird/flyingbird.cs
+++ FlyingBird/FlyingBird/flyingbird.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FlyingBird
 {
     [TestClass]
-    public class UnitTest1
+    public class flyingbird
     {
         [TestMethod]
         public void TestMethod1()
         {
+            int distance = CalculateFlyingDistance(60, 240);
+            Assert.AreEqual(120, distance);
+        }
+        int CalculateFlyingDistance(int trainSpeed, int trainDistance)
+        {
+
+                        int birdFlyingTime = trainDistance / (trainSpeed * 2) - ((trainDistance / 4) / trainSpeed);
+                         return trainSpeed * 2 * birdFlyingTime;
         }
     }
 }
