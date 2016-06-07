4bb926603ff85a13c48b5c5eeac0f3a2de07970b
flaviu83 <juniormind2016@gmail.com>
2016-06-07 13:13:23 +0300

    Goats

---
+++ Goats/Goats/Goats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Goats
+{
+    [TestClass]
+    public class Goats
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal amountHay = CalculateKgHay(2, 5, 50, 7, 4);
+            Assert.AreEqual(140, amountHay);
+        }
+        
+        decimal CalculateKgHay(int nrDays, int nrGoats, decimal kgHay, int nrdaysanswer, int nrgoatsanswer)
+        {
+            decimal hayGoatPerDay = kgHay / (nrDays * nrGoats);
+             return hayGoatPerDay * nrgoatsanswer * nrdaysanswer;
+        }
+    }
+}
