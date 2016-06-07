481dcdb1be99cfa5371a466e64e8be4aa6b9da97
flaviu83 <juniormind2016@gmail.com>
2016-06-07 13:59:49 +0300

    Mushrooms
    Number of Red Mushrooms

---
+++ Mushrooms/Mushrooms/mushrooms.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class mushrooms
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int red = RedMushrooms(2, 4);
+            Assert.AreEqual(8, red);
+        }
+        int RedMushrooms(int whiteMushrooms, int x)
+        {
+            int redMushrooms = whiteMushrooms * x;
+            return redMushrooms;
+        }
+    }
+}
