c17c9b049d46e023ed1a01efc2fd6b092d41a33c
flaviu83 <juniormind2016@gmail.com>
2016-06-24 02:35:59 +0300

    FarmLand

---
+++ FarmLand/FarmLand/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmLand
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            double dimension = InitialDimension(770000, 230);
+                         Assert.AreEqual(592900, dimension);
+        }
+        double InitialDimension(int newArea, int lengthOfNewArea)
+         { 
+             int initialLength = 0; 
+             double initialArea = newArea - initialLength * lengthOfNewArea; 
+             initialArea=initialLength* initialLength; 
+ 
+ 
+             double delta = Math.Pow(lengthOfNewArea, 2) + 4 * newArea; 
+             initialArea = (-lengthOfNewArea + Math.Sqrt(delta)) / 2; 
+ 
+ 
+                return Math.Pow(initialArea, 2); 
+        
+          }
+}
+}
