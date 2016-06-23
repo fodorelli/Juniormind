b9b730ea8eff33c36d6e98f22fac1269077482a1
flaviu83 <juniormind2016@gmail.com>
2016-06-23 17:22:54 +0300

    Parquet

---
+++ Parquet/Parquet/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parquet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            double totalnoofParquet = noParquet(4, 4, 2, 2);
+            Assert.AreEqual(5, totalnoofParquet);
+        }
+        double noParquet(int n, int m, int a, int b)
+        { 
+           return (double)(n/a)* (double)(m/b)*115/100; 
+       }
+}
+}
