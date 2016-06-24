075909db35719dbad6cb9e595e27281c719caefd
flaviu83 <juniormind2016@gmail.com>
2016-06-24 13:31:59 +0300

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod] 
+         public void TestBuzz()
+        {
+                        Assert.AreEqual("BUZZ", FizzBuzz(10));
+                    } 
+         [TestMethod] 
+         public void TestFizz()
+         { 
+             Assert.AreEqual("FIZZ", FizzBuzz(12)); 
+         } 
+         [TestMethod] 
+         public void TestFizzBuzz()
+         { 
+             Assert.AreEqual("FIZZBUZZ", FizzBuzz(30)); 
+         }
+
+string FizzBuzz(int Number)
+         { 
+ 
+ 
+            if (Number % 5 == 0 && Number % 3 == 0) 
+                return "FIZZBUZZ"; 
+ 
+ 
+            if (Number % 5 == 0) 
+                return "BUZZ"; 
+ 
+ 
+             if (Number % 3 == 0) 
+                return "FIZZ"; 
+
+ 
+            return Number.ToString(); 
+         }
+}
+}
