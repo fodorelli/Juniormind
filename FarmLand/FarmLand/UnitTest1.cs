0cb9a9813b68c5c53797158ed3bb074629014f4f
flaviu83 <juniormind2016@gmail.com>
2016-07-07 00:18:23 +0300

    FarmLand

--- FarmLand/FarmLand/UnitTest1.cs
+++ FarmLand/FarmLand/UnitTest1.cs
@@ -9,22 +9,18 @@ namespace FarmLand
         [TestMethod]
         public void Test1()
         {
-            double dimension = InitialDimension(770000, 230);
-                         Assert.AreEqual(592900, dimension);
+            
+                      Assert.AreEqual(770, InitialDimension(770000, 230));
         }
         double InitialDimension(int newArea, int lengthOfNewArea)
-         { 
-             int initialLength = 0; 
-             double initialArea = newArea - initialLength * lengthOfNewArea; 
-             initialArea=initialLength* initialLength; 
- 
- 
-             double delta = Math.Pow(lengthOfNewArea, 2) + 4 * newArea; 
-             initialArea = (-lengthOfNewArea + Math.Sqrt(delta)) / 2; 
- 
- 
-                return Math.Pow(initialArea, 2); 
-        
-          }
+         {
+            double Delta = lengthOfNewArea * lengthOfNewArea + 4 * newArea;
+
+            double x1 = (-lengthOfNewArea + Math.Sqrt(Delta)) / 2;
+            double x2 = (-lengthOfNewArea + Math.Sqrt(Delta)) / 2;
+
+            return x1 > 0 ? x1 : x2;
+
+        }
 }
 }
