c4e51b7d17b64b5df8a2bbcbe34f318263003c26
flaviu83 <juniormind2016@gmail.com>
2016-07-18 23:48:14 +0300

    ChessBoard

---
+++ ChessBoard/ChessBoard/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessBoard
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestChessBoard8x8()
+        {
+            Assert.AreEqual(204, CalculateTheSquaresOnAChessboard(8));
+        }
+        int CalculateTheSquaresOnAChessboard(int lenght)
+          {  
+ 
+           int noOfSquares = 0;  
+           for (int i = 1; i <= lenght; i++)  
+               noOfSquares += (i* i);  
+           return noOfSquares;  
+          }
+}
+}
