8c660bef64163aa34e85d58d5911f4388be7a73a
flaviu83 <juniormind2016@gmail.com>
2016-08-04 00:08:11 +0300

    Prefix

---
+++ Prefix/Prefix/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FindCommonPrefix()
+        {
+            Assert.AreEqual("aaa", CalculatePrefix("aaabba", "aaaaaabb"));
+        }
+        [TestMethod]
+        public void FindCommonPrefix2()
+        {
+            Assert.AreNotEqual("cdf", CalculatePrefix("aaabba", "aaaaaabb"));
+        }
+        public string CalculatePrefix(string firstWord, string secondWord)
+         { 
+
+ 
+             string commonPrefix = string.Empty; 
+ 
+ 
+             for (int i = 0; i<firstWord.Length && i<secondWord.Length; i++) 
+            { 
+                 if (firstWord[i] != secondWord[i]) break; 
+
+ 
+               else  
+                     commonPrefix += firstWord[i];  
+ 
+ 
+           } 
+
+ 
+            return commonPrefix; 
+ 
+ 
+       } 
+
+    }
+}
