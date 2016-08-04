5aa6b4639ae1886968f5eca381984ec668231073
flaviu83 <juniormind2016@gmail.com>
2016-08-05 01:45:11 +0300

    Lunch

---
+++ Lunch/Lunch/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lunch
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(12, GetNumberOfDaysUntilTheNextLunchMeeting(4, 6));
+        }
+        int LunchMeeting(int a, int b)
+         { 
+             while (a != b) 
+             { 
+                 if (a > b) 
+ 
+ 
+                    a -= b; 
+ 
+ 
+                 else 
+                     b -= a; 
+
+ 
+             } 
+             return a; 
+         } 
+ 
+ 
+        int GetNumberOfDaysUntilTheNextLunchMeeting(int hisDay, int myDay)
+        { 
+
+ 
+ 
+ 
+            return hisDay* myDay / LunchMeeting(hisDay, myDay);  
+         } 
+
+    }
+}
