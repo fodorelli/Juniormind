5faef20e761c0f8daa9fefdc2da35f008c7420c0
flaviu83 <juniormind2016@gmail.com>
2016-08-05 16:17:23 +0300

    Cube

---
+++ cube/cube/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cube
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(192, GetCubeNumber(1));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(442, GetCubeNumber(2));
+        }
+        int GetCubeNumber(int wantedNumber)
+         { 
+             int counter = 0; 
+             for(int i = 0; i<1000; i++) 
+             { 
+                 if(Math.Pow(i, 3) % 1000 == 888) 
+                 { 
+                     counter++; 
+                     if (counter == wantedNumber) 
+                         return i; 
+                 } 
+             } 
+             return 0; 
+         } 
+
+    }
+}
